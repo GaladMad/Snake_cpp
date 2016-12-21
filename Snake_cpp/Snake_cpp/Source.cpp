2161aa616c59614350b8e8e796a076a779d347a2
GaladMad <kam.fedio@gmail.com>
2016-12-21 20:18:22 +0100

    Second commit

--- Snake_cpp/Snake_cpp/Source.cpp
+++ Snake_cpp/Snake_cpp/Source.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int main() {
 	cout << "It will be a snake" << endl;
+	cout << "new change" << endl;
+	cout << "new text ones again" << endl;
 	system("PAUSE");
 	return 0;
 }
